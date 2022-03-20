67ae6b38e336f18deb8e7b21af694557fd6511e8
Florent C. L. M. Michel <56166507+FlorentCLMichel@users.noreply.github.com>
2022-03-20 18:10:15 +0000

    add an example with negative g

---
+++ src/examples/1d_gaussian_neg_g.rs
@@ -0,0 +1,51 @@
+use nonlinear_schrodinger::prelude::*;
+use nonlinear_schrodinger::plotters::plot_1d;
+
+const COL1: (u8, u8, u8) = (0, 0, 255);
+const COL2: (u8, u8, u8) = (0, 255, 0);
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // parameters
+    let shape: Vec<usize> = vec![512];
+    let steps: Vec<R> = vec![0.025];
+    let potential_fun = |_x: &[R]| 0.;
+    let g = |x: R| -x;
+    
+    // initial condition
+    let amplitude = 2_f64.sqrt();
+    let nx = shape[0];
+    let dx = steps[0];
+    let mut psi = (0..nx).map(|i| 
+                              C::new(- 2_f64.powf(-0.5) * ((i as R - (nx/2) as R) * dx).powi(2), 0.).exp()
+                              * amplitude
+                          )
+                         .collect::<Vec<C>>();
+    
+    // build the solver
+    let mut solver = Solver::new(shape, steps, Box::new(potential_fun), Box::new(g))?;
+     
+    // evolution
+    let n_steps = 10;
+    let dt = 0.001;
+    let nt = 1000;
+    let mut densities = vec![
+        psi.iter().map(|x| x.abs2()).collect::<Vec<R>>()
+    ];
+    println!("Mass at step 0: \t{}", solver.mass(&psi)?);
+    for i in 0..n_steps {
+        solver.evolve(&mut psi, dt, nt)?;
+        densities.push(psi.iter().map(|x| x.abs2()).collect::<Vec<R>>());
+        println!("Mass at step {}: \t{}", i+1, solver.mass(&psi)?);
+    }
+    
+    // plot the densities
+    plot_1d(&vec![solver.grid.iter().map(|p| p[0]).collect()], 
+            &densities, 
+            "Results/1d_gaussian_neg_g",
+            "Density", "x", "ρ", 
+            &(0..=n_steps).map(|i| format!("t = {:.2}", ((i*nt) as R) * dt)).collect::<Vec<String>>(),
+            COL1, COL2)?;
+
+    Ok(())
+}
